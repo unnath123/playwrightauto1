4385203ea215bd755a1b71988dff3f78bd47aac9
unnath123 <unnathpgowda@gmail.com>
2024-09-22 22:12:41 +0530

    first commit after ages

---
+++ tests/Firsttest.spec.js
@@ -0,0 +1,23 @@
+const { test, expect } = require('@playwright/test');
+
+test("first test", async( {page})=>{
+    await page.goto("https://www.airbnb.co.in/");
+
+    await page.locator("#bigsearch-query-location-input").fill("Benga")
+
+    const inputcontent = await page.locator("#bigsearch-query-location-input").inputValue();
+
+    const suggestionValue =  await page.locator("div[id=bigsearch-query-location-listbox]");
+    const suggestionValues = await suggestionValue.locator("div[role='option']");
+
+    const num = await suggestionValues.count()
+
+    console.log("number of elements = ",num);
+
+    for (let i = 0; i < 3; i++) {
+        const text = await suggestionValues.nth(i).textContent(); // Accessing the i-th option
+        console.log("Option " + (i + 1) + ": " + text);
+    }
+
+    console.log("this is the text",inputcontent);
+})
