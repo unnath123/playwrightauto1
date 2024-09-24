dbe5f8527f384a4a857963455751f0b9586e3614
unnath123 <unnathpgowda@gmail.com>
2024-09-25 00:16:20 +0530

    multiple rows from table

--- tests/Secondtest.spec.js
+++ tests/Secondtest.spec.js
@@ -3,30 +3,52 @@ const { test, expect } = require('@playwright/test');
 test("second test", async({page})=>{
     await page.goto("https://testautomationpractice.blogspot.com/");
 
-    const table = await page.locator("table[name='BookTable']");
+    const table = await page.locator("table[id='productTable']");
 
-    const cols = await table.locator("th").count();
+    const cols = await table.locator("thead tr th");
+    const ColsCount = await cols.count();
 
-    console.log("this is the columns count = ",cols)
+    console.log("this is the columns count = ",ColsCount)
 
-    const rows = await table.locator("tr")
+    const rows = await table.locator("tbody tr")
 
-    console.log("this is the rows count = ", await rows.count());
+    const RowCount = await rows.count();
 
-    const matchedRow = await rows.filter({
-        has: page.locator("td"),
-        hasText: "Animesh"
-    })
+    console.log("this is the rows count = ", RowCount);
 
-    console.log("rows in answer ROW",await matchedRow.count());
+    // const matchedRow = await rows.filter({
+    //     has: page.locator("td"),
+    //     hasText: "Animesh"
+    // })
 
-    console.log("matched ROW", matchedRow)
+   // console.log("rows in answer ROW",await matchedRow.count());
+   // console.log("matched ROW", matchedRow)
 
-    const arr = matchedRow.locator("td")
+   await gothillla(rows, page, "Product 1")
+   await gothillla(rows, page, "Product 2")
+   await gothillla(rows, page, "Product 3")
 
-    for(let i=0; i<4; i++){
-       const ans =  await arr.nth(i).textContent();
-       console.log(ans)
-    }
+    // const arr = matchedRow.locator("td")
+    // for(let i=0; i<4; i++){
+    //    const ans =  await arr.nth(i).textContent();
+    //    console.log(ans)
+    // }
+     
 
 })
+
+async function gothillla(rows, page, identifier){
+    const matchedRow = await rows.filter({
+        has: page.locator("td"),
+        hasText: identifier
+    })
+    const arr = matchedRow.locator("td")
+    await anotherOne(arr)
+}
+
+async function anotherOne(arr){
+    for(let i=0; i<3; i++){
+       const ans =  await arr.nth(i).textContent();
+       console.log(ans);
+    }
+}
